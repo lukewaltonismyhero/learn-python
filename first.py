9b3a7b28dd3f60ee243e4e38dfabc2388046a9c6
Esther N <esthernam@gmail.com>
2013-04-17 20:59:27 -0700

    First assigment created

--- first.py
+++ first.py
@@ -1,3 +1,18 @@
+print '\n'
+
 import this
 
+print '\n'
+raw_input("Press 'Enter' to continue:")
+print '_-*-_'*16
+print '\n'
+print "You just uncovered the 'secret' guide for programmers who want to think Pythonically when coding."
 
+print '\n'
+
+print "Your next assignment is to write a Python script that prints a statement that you live by. You can make it as pretty or as plain as you please."
+print "\t1. Run it to make sure it works"
+print "\t2. Run 'git add FILENAME' to add the file to version control"
+print "\t3. Run 'git commit -m 'MESSAGE DESCRIBING THE CHANGES YOU MADE' to commit your change."
+print "\t4. Run the command 'git push origin master' to push your change to Github.com"
+print "(\__/)\n(='.'=)\n(\")_(\")    Make me proud!"
