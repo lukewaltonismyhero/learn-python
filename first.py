58ab9348177c98d707a74cefb33701addf1573ac
Esther <esther@meringue.(none)>
2013-04-25 09:27:53 -0700

    Polishing off assignment `

--- first.py
+++ first.py
@@ -1,20 +1,13 @@
 # This is the first class assignment. You could cheat and read below to see what it is, but it's much more fun to run the script in the command line! Then you can see what Line 20 is all about =)
+print '_-*-_'*16
+print '\n'
 import this
 
 print '\n'
-
-# Don't worry about what raw_input does. We will cover it in the next class
-raw_input("Press 'Enter' to continue:")
-
 print '_-*-_'*16
 print '\n'
-print "You just uncovered the 'secret' guide for programmers who want to think Pythonically when coding."
+print "You've just uncovered the 'secret' guide for programmers who want to think Pythonically when coding!"
 
 print '\n'
-print "Your next assignment is to write a Python script that prints a statement that you live by. You can make it as pretty or as plain as you please."
-print "\t1. Run it to make sure it works"
-print "\t2. Run 'git add FILENAME' to add the file to version control"
-print "\t3. Run 'git commit -m 'MESSAGE DESCRIBING THE CHANGES YOU MADE' to commit your change."
-print "\t4. Run the command 'git push origin master' to push your change to Github.com"
 # What is this?? Oh me, oh my!
-print "(\__/)\n(='.'=)\n(\")_(\")    Make me proud!"
+print "(\__/)\n(='.'=)\n(\")_(\"i)"
