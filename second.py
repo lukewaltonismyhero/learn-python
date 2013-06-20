45d13524baa70232a90be296ae6715232760c25c
Esther Nam <enam@cars.com>
2013-06-19 23:49:39 -0700

    adding second assingment

---
+++ second.py
@@ -0,0 +1,38 @@
+CLASSROOM = [
+('Esther', 'pizza', 'blue', 'sitedev')
+('Andrew', 'dumplings', 'green', 'sitedev')
+('Esther', 'ramen', 'red', 'sem')
+('Connie', 'burrito', 'green', 'sem')
+('Alex', 'cheesecake', 'purple', 'sem')
+('Bobby', 'acai bowl', 'orange', 'ops')
+('Eric', 'sandwich', 'black', 'sem')
+('Cameron', 'pasta', 'green', 'sem')
+('Courtney', 'sandwich', 'red', 'sitedev')
+('Kevin', 'calzone', 'yellow', 'sem')
+]
+
+# this function should return a boolean, True, if the team member is in SEM
+def is_sem_tem(person):
+    pass
+
+# This function should record the number of times a given color is picked
+# HINT: What would be the best way to keep track of how many times a color has been picked?
+def record_favorite_color(color_count, person):
+    pass
+ 
+ 
+ # This function should record the number of poeple in each department
+def department_count(sem_count):
+    pass
+     
+ 
+"""
+ This function should print the following stats:
+ 1. The total number of people in SEM
+ 2. The most popular color
+ 3. 
+"""
+ def classroom_stats():
+    pass
+    
+    
